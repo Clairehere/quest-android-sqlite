f8c3fee5042a6482c1dc3e11732fce9878a4c5e0
Clairehere <b_claire@hotmail.com>
2018-04-04 17:02:22 +0200

    SQL Person et Room

--- app/src/main/java/fr/wildcodeschool/roomreservation/RoomListActivity.java
+++ app/src/main/java/fr/wildcodeschool/roomreservation/RoomListActivity.java
@@ -1,5 +1,7 @@
 package fr.wildcodeschool.roomreservation;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
@@ -25,6 +27,32 @@ public class RoomListActivity extends AppCompatActivity {
 
         // TODO : load rooms from database
 
+        DbHelper mDbHelper = new DbHelper(RoomListActivity.this);
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        String[] projection = {
+                DBContract.RoomEntry._ID,
+                DBContract.RoomEntry.COLUMN_NAME_NAME,
+        };
+
+
+        Cursor cursor = db.query(
+                DBContract.RoomEntry.TABLE_NAMES,
+                projection,
+                null, null, null, null, null
+        );
+
+        while (cursor.moveToNext()) {
+            long id = cursor.getLong(cursor.getColumnIndexOrThrow(DBContract.RoomEntry._ID));
+            String name = cursor.getString(cursor.getColumnIndexOrThrow(DBContract.RoomEntry.COLUMN_NAME_NAME));
+
+
+            RoomModel roomModel = new RoomModel(id, name);
+            roomModels.add(roomModel);
+        }
+        cursor.close();
+
         return roomModels;
+
     }
 }
